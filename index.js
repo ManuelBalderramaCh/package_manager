7efa846dbee28e2e9e16ed40d9d11857230e93ef
= <=>
2023-02-08 09:25:26 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const log4js = require('log4js');
+let logger = log4js.getLogger();
+logger.level = "debug";
+logger.debug("Some debug messages");
+
+function sumar(x,y){
+    return x + y;
+}
+
+module.exports = sumar;
+
+let variable_uno = 20;
---
+++ test/indexTest.js
@@ -0,0 +1,20 @@
+const sumar = require("../index");
+const assert = require('assert');
+
+//Pruebas unitarias, funcionales, integracion y estres.
+// Como se escribe una prueba?
+// 50 % -> 1 prueba correcta y 1 prueba que falle.
+// Asserts = Afirmacion
+
+describe("Probar la suma de dos numeros", ()=>{
+    //Afirmamos que cinco mas cinco son 10
+    it("Cinco mas cinco es 10", ()=>{
+        assert.equal(10,sumar(5,5))
+    });
+    it("Cinco mas cinco no son 55",()=>{
+        assert.notEqual(55,sumar(5,5))
+    });
+    //Afirmamos que 5 mas 5 no son 55
+});
+
+//Funcion anonima () => {} ------ en JS
